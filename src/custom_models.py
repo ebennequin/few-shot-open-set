56bf21afa2214758b0410a382906c010a5e6d934
ebennequin <etienne.bennequin@gmail.com>
2021-11-15 18:03:59 +0100

    Clean custom ResNet

--- src/custom_models.py
+++ src/custom_models.py
@@ -5,6 +5,7 @@ From https://github.com/fiveai/on-episodes-fsl/blob/master/src/models/ResNet.py
 
 import torch.nn as nn
 import torch
+from torchvision.models.resnet import Bottleneck, BasicBlock, conv1x1
 
 __all__ = [
     "resnet10",
@@ -17,117 +18,33 @@ __all__ = [
 ]
 
 
-def conv3x3(in_planes, out_planes, stride=1):
-    """3x3 convolution with padding"""
-    return nn.Conv2d(
-        in_planes, out_planes, kernel_size=3, stride=stride, padding=1, bias=False
-    )
-
-
-def conv1x1(in_planes, out_planes, stride=1):
-    """1x1 convolution"""
-    return nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, bias=False)
-
-
-class BasicBlock(nn.Module):
-    expansion = 1
-
-    def __init__(self, inplanes, planes, stride=1, downsample=None):
-        super(BasicBlock, self).__init__()
-        self.conv1 = conv3x3(inplanes, planes, stride)
-        self.bn1 = nn.BatchNorm2d(planes)
-        self.relu = nn.ReLU(inplace=True)
-        self.conv2 = conv3x3(planes, planes)
-        self.bn2 = nn.BatchNorm2d(planes)
-        self.downsample = downsample
-        self.stride = stride
-
-    def forward(self, x):
-        identity = x
-
-        out = self.conv1(x)
-        out = self.bn1(out)
-        out = self.relu(out)
-
-        out = self.conv2(out)
-        out = self.bn2(out)
-
-        if self.downsample is not None:
-            identity = self.downsample(x)
-
-        out += identity
-        out = self.relu(out)
-
-        return out
-
-
-class Bottleneck(nn.Module):
-    expansion = 4
-
-    def __init__(self, inplanes, planes, stride=1, downsample=None):
-        super(Bottleneck, self).__init__()
-        self.conv1 = conv1x1(inplanes, planes)
-        self.bn1 = nn.BatchNorm2d(planes)
-        self.conv2 = conv3x3(planes, planes, stride)
-        self.bn2 = nn.BatchNorm2d(planes)
-        self.conv3 = conv1x1(planes, planes * self.expansion)
-        self.bn3 = nn.BatchNorm2d(planes * self.expansion)
-        self.relu = nn.ReLU(inplace=True)
-        self.downsample = downsample
-        self.stride = stride
-
-    def forward(self, x):
-        identity = x
-
-        out = self.conv1(x)
-        out = self.bn1(out)
-        out = self.relu(out)
-
-        out = self.conv2(out)
-        out = self.bn2(out)
-        out = self.relu(out)
-
-        out = self.conv3(out)
-        out = self.bn3(out)
-
-        if self.downsample is not None:
-            identity = self.downsample(x)
-
-        out += identity
-        out = self.relu(out)
-
-        return out
-
-
 class ResNet(nn.Module):
     def __init__(
         self,
         block,
         layers,
-        widths=[64, 128, 256, 512], # TODO: est-ce que c'est pris en compte ?
-        feature_dim=512,
+        widths=None,
         num_classes=1000,
-        projection=False,
         zero_init_residual=False,
-        drop_rate=0,
         use_fc=False,
     ):
         super(ResNet, self).__init__()
-        if drop_rate > 0:
-            raise NotImplementedError()
-        self.use_fc = use_fc
-        self.projection = projection
+        if widths is None:
+            widths = [64, 128, 256, 512]
+
         self.inplanes = 64
-        self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(64)
+        self.use_fc = use_fc
+
+        self.conv1 = nn.Conv2d(
+            3, self.inplanes, kernel_size=3, stride=1, padding=1, bias=False
+        )
+        self.bn1 = nn.BatchNorm2d(self.inplanes)
         self.relu = nn.ReLU(inplace=True)
         self.layer1 = self._make_layer(block, widths[0], layers[0])
         self.layer2 = self._make_layer(block, widths[1], layers[1], stride=2)
         self.layer3 = self._make_layer(block, widths[2], layers[2], stride=2)
         self.layer4 = self._make_layer(block, widths[3], layers[3], stride=2)
         self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
-        if self.projection:
-            self.proj1 = nn.Linear(widths[3] * block.expansion, feature_dim)
 
         if self.use_fc:
             self.fc = nn.Linear(widths[3] * block.expansion, num_classes)
@@ -165,24 +82,22 @@ class ResNet(nn.Module):
 
         return nn.Sequential(*layers)
 
-    def forward(self, x, use_fc=False, cat=False):
-        x = self.conv1(x)
-        x = self.bn1(x)
-        x = self.relu(x)
-
-        x = self.layer1(x)
-        x = self.layer2(x)
-        x = self.layer3(x)
-        x = self.layer4(x)
-
-        x = self.avgpool(x)
-
-        x = x.view(x.size(0), -1)
+    def forward(self, x, use_fc=False):
+        features = torch.flatten(
+            self.avgpool(
+                self.layer4(
+                    self.layer3(
+                        self.layer2(self.layer1(self.relu(self.bn1(self.conv1(x)))))
+                    )
+                )
+            ),
+            1,
+        )
 
         if use_fc:
-            x = self.fc(x)
+            return self.fc(x)
 
-        return x
+        return features
 
 
 def resnet10(**kwargs):

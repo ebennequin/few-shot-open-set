eaf9c75a36c0ca084a6708a39bf3f23b56a59628
ebennequin <etienne.bennequin@gmail.com>
2022-01-19 17:18:48 +0100

    Create new pipeline for computing features

--- pipelines/compute_features/compute_features.py
+++ pipelines/compute_features/compute_features.py
@@ -7,7 +7,9 @@ from pathlib import Path
 from loguru import logger
 import torch
 import typer
+from torch import nn
 
+from pipelines.compute_features.params import BATCH_SIZE, BACKBONE, DEVICE, N_WORKERS
 from src.constants import (
     BACKBONES,
     FEATURES_DIR,
@@ -17,58 +19,65 @@ from src.utils.utils import compute_features
 
 
 def main(
-    backbone: str,
     dataset: str,
     weights: Path,
-    split: str = "test",
     output_file: Path = None,
-    batch_size: int = 1024,
-    avg_pool: bool = True,
-    device: str = "cuda",
 ):
     """
     Compute all features of given dataset images with the given model and dump them in given
     output pickle file.
     Args:
-        backbone: what model to train. Must be a key of constants.BACKBONES.
         dataset: what dataset to train the model on.
         weights: path to trained weights
-        split: which split of the dataset to infer on
         output_file: where to dump the pickle containing the features
-        batch_size: the batch size
-        avg_pool: whether to return, for each instance, a 1-d vector of pooled veatures,
-            or a 3D feature map (n_channels, height, width)
-        device: what device to train the model on
     """
-    logger.info("Fetching data...")
-    data_loader = get_classic_loader(
-        dataset, split=split, batch_size=batch_size, n_workers=20
-    )
-
     logger.info("Building model...")
-    feature_extractor = BACKBONES[backbone](avg_pool=avg_pool).to(device)
+    feature_extractor = BACKBONE(avg_pool=False).to(DEVICE)
     feature_extractor.load_state_dict(
         strip_prefix(torch.load(weights), "backbone."), strict=False
     )
     feature_extractor.eval()
 
-    logger.info("Computing features...")
-    features, labels = compute_features(feature_extractor, data_loader, device=device)
+    logger.info("Starting inference on test set...")
+    features, labels = infer_on_dataset(
+        dataset=dataset, split="test", model=feature_extractor
+    )
 
-    logger.info("Packing by class...")
+    logger.info("Packing test set features by class...")
     packed_features = {
         class_integer_label: features[np.where(labels == class_integer_label)]
         for class_integer_label in set(labels)
     }
 
+    logger.info("Starting inference on train set...")
+    train_features, _ = infer_on_dataset(
+        dataset=dataset, split="train", model=feature_extractor
+    )
+    average_train_features = train_features.mean(0)
+
     if output_file is None:
-        output_file = (
-            FEATURES_DIR / dataset / split / weights.with_suffix(".pickle").name
-        )
+        output_file = FEATURES_DIR / f"{dataset}.pickle"
     output_file.parent.mkdir(parents=True, exist_ok=True)
     with open(output_file, "wb") as stream:
-        pickle.dump(packed_features, stream, protocol=pickle.HIGHEST_PROTOCOL)
+        pickle.dump(
+            {
+                "test_set_features": packed_features,
+                "average_train_set_features": average_train_features,
+            },
+            stream,
+            protocol=pickle.HIGHEST_PROTOCOL,
+        )
     logger.info(f"Dumped features in {output_file}")
+
+
+def infer_on_dataset(dataset: str, split: str, model: nn.Module):
+    logger.info("Fetching data...")
+    data_loader = get_classic_loader(
+        dataset, split=split, batch_size=BATCH_SIZE, n_workers=N_WORKERS
+    )
+
+    logger.info("Computing features...")
+    return compute_features(model, data_loader, device=DEVICE)
 
 
 def strip_prefix(state_dict: OrderedDict, prefix: str):
---
+++ pipelines/compute_features/params.py
@@ -0,0 +1,16 @@
+from pydoc import locate
+
+import yaml
+from loguru import logger
+
+PARAMETERS_FILE = "params.yaml"
+
+
+with open(PARAMETERS_FILE) as file:
+    logger.info(f'Parsing parameters from "{PARAMETERS_FILE}"')
+    params = yaml.safe_load(file)
+
+BACKBONE = locate(params["backbone"])
+BATCH_SIZE = params["batch_size"]
+N_WORKERS = params["n_workers"]
+DEVICE = params["device"]
